b4d94d332f6818b151ff645658b5af6792bbff68
jonvhauw <jonas.vanhauwe@ugent.be>
2025-04-10 12:50:24 +0200

    Added functions for generating rectangle images

--- functions.py
+++ functions.py
@@ -12,7 +12,13 @@ from tensorflow.keras import layers, losses
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.models import Model
 
+def load_mnist_data():
+  #Loading mnist dataset
+  (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
+  x_train = x_train.astype('float32') / 255.
+  x_test = x_test.astype('float32') / 255.
+  return x_train, y_train, x_test, y_test
 
 #From tensorflow tutorial
 #https://www.tensorflow.org/tutorials/generative/autoencoder
@@ -46,6 +52,12 @@ def train_autoencoder(x_train, x_test, latent_dim, optimizer='adam', loss=losses
                 validation_data=(x_test, x_test))
   return autoencoder
 
+def get_reconstructed_img(autoencoder, x_test):
+  #Get encoded and decoded images
+  encoded_imgs = autoencoder.encoder(x_test).numpy()
+  decoded_imgs = autoencoder.decoder(encoded_imgs).numpy()
+  return encoded_imgs, decoded_imgs
+
 def plot_reconstruction(x_test, decoded_imgs, n=10):
     plt.figure(figsize=(20, 4))
     for i in range(n):
@@ -65,3 +77,21 @@ def plot_reconstruction(x_test, decoded_imgs, n=10):
         ax.get_xaxis().set_visible(False)
         ax.get_yaxis().set_visible(False)
     plt.show()
+
+def generate_rectangle(label, size=28):
+  rectangle = np.zeros((size, size))
+  if label == 0:
+    start_x = np.random.randint(0, 22)
+    start_y = np.random.randint(0, 17)
+    rectangle[start_y:start_y+10, start_x:start_x+5] = 1
+  if label == 1:
+    start_x = np.random.randint(0, 17)
+    start_y = np.random.randint(0, 22)
+    rectangle[start_y:start_y+5, start_x:start_x+10] = 1
+  return rectangle
+
+def get_rectangle_img(length=70000, seed=1234):
+  np.random.seed(seed)
+  labels = np.random.randint(0, 2, size=length)
+  rectangles = np.array([generate_rectangle(i) for i in labels])
+  return rectangles
